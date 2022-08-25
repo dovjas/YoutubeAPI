c029b439b83c2fa37cd700903a7c85e97ad4cdf8
Dovydas J <dovydas.jaseliunas@gmail.com>
2022-08-25 16:11:32 +0300

    Handling form submit

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,16 +1,34 @@
-import React from "react"
+import React,{Component} from "react"
 
-function SearchField() {
-  return (
-    <div className="search-bar ui segment">
-      <form className="ui form">
-        <div className="field">
-          <label>VIDEO</label>
-          <input type="text" placeholder="Search video" />
-        </div>
-      </form>
-    </div>
-  )
+class SearchField extends React.Component {
+  state = {term: ''};
+
+
+  onInputChange = event=> {
+    this.setState({term: event.target.value});
+  }
+
+  onFormSubmit = event =>{
+    event.preventDefault();
+
+  }
+
+  render(){
+    return (
+      <div className="search-bar ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>VIDEO</label>
+            <input type="text" 
+            placeholder="Search video" 
+            value={this.state.term}
+            onChange={this.onInputChange}
+            />
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default SearchField
